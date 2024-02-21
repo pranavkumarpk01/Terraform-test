f83064c1b94c46793af0da522aee80a29f4ff939
pranavkumarpk01 <pranavkumarpk0107@gmail.com>
2024-02-21 12:40:53 +0530

    main file

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ provider "azurerm" {
 }
 #creating a resource group
 resource "azurerm_resource_group" "resource_group_test" {
-    name = "rg-terraform-demo"
+    name = "rg-terraform-dem"
     location = "South India"
   }
 #creating a storage acccount
 resource "azurerm_storage_account" "storage_account" {
-  name                     ="tstorage0107"
+  name                     ="tstorage010708"
   resource_group_name      = azurerm_resource_group.resource_group_test.name
   location                 = azurerm_resource_group.resource_group_test.location
   account_tier             = "Standard"
